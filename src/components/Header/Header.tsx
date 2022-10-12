03990e9e01bda9743c1d7d9b5777bb317b4df331
Guilherme Camboim <105326604+guilhermecamboim@users.noreply.github.com>
2022-10-12 18:39:10 -0300

    Melhorando header da aplicacão.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,6 +5,12 @@ export function Header(){
   return (
     <header className={styles.header}>
       <img src={logoImg} alt="Logotipo do Ler Abre Caminhos"/>
+      <h1>@LerAbreCaminhos</h1>
+      <div className="containerSelection">
+        <a>Sobre</a>
+        <a>Estou lendo</a>
+        <a>Contato</a>
+      </div>
     </header>
   )
 }
