8e8bc7500034deb47f156368726aa829aae56903
Guilherme Camboim <105326604+guilhermecamboim@users.noreply.github.com>
2022-10-19 21:46:21 -0300

    Implementando Navlink no projeto.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,14 @@
 import styles from './styles.module.css';
 import logoImg from '../../../assets/logo.png';
+import { NavLink } from 'react-router-dom';
 
 export function Header(){
   return (
     <header className={styles.header}>
-      <img src={logoImg} alt="Logotipo do Ler Abre Caminhos"/>
-      <h1>@LerAbreCaminhos</h1>
+      <NavLink to="/" title="Home">
+        <img src={logoImg} alt="Logotipo do Ler Abre Caminhos"/>
+      </NavLink>
+        <h1>@LerAbreCaminhos</h1>
       <div className="containerSelection">
         <a>Sobre</a>
         <a>Estou lendo</a>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { Sidebar } from '../../components/Sidebar/Sidebar';
 
 const posts = [
@@ -10,6 +10,7 @@ const posts = [
       name: 'Guilherme Camboim',
       role: 'Autor',
     },
+    title: 'Voce sabe o que é amnésia de paisagem?',
     content: [
       {type: 'paragraph', content:'Em outras palavras, os danos não ocorreram do dia para noite, mas sim durante décadas. Essas pequenas mudanças na paisagem, que eram rotineiras, tornaram-se quase imperceptíveis ao olhar mais apurado (amnésia de paisagem) até que o Colapso - eternizado por Jared Diamond neste livro - estivesse formado.'},
       {type: 'paragraph', content:'A moral da história? Segundo Rutger Bregman "A moral diz respeito a nós mesmos. Se pusermos a Ilha de Páscoa e o planeta Terra lado a lado, veremos alguns paralelos inquietantes. Vamos considerar: a Ilha de Páscoa é uma manchinha no vasto oceano; a Terra é uma manchinha na imensidão do cosmos. Os ilhéus não tinham barcos para fugir; nós não temos foguetes para nos levar daqui. A Ilha de Páscoa foi desmatada e superpovoada; nosso planeta está ficando poluído e superaquecido".'},
@@ -25,6 +26,7 @@ const posts = [
       name: 'Danielle Souza',
       role: 'Autor',
     },
+    title: 'Post 2',
     content: [
       {type: 'image', content:'https://images.unsplash.com/photo-1665425252293-af1d4696829f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60'},
       {type: 'image', content:'https://images.unsplash.com/photo-1665474242672-4572ac74ef6f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60'},
@@ -38,7 +40,7 @@ const posts = [
   },
 ];
 
-/* const navigate = useNavigate(); */
+
 
 export function Home() {
   return (
@@ -46,9 +48,11 @@ export function Home() {
       <Sidebar/>
       {posts.map(item => (
         <>
-        <h1>{item.author.name}</h1>
+        <h1>{item.title}</h1>
         <img />
-        <button style={{width: 40, height: 40, background:"blue"}} /* onClick={() => navigate(`/post/${item.id}`)} *//>
+        <NavLink to={`/post/${item.id}`} title="Post">
+          <button style={{width: 40, height: 40, background:"blue"}}/>
+        </NavLink>
         </>
       ))}
     </>
