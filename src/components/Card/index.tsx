07a4f4f80a58f6304d640226edce8f0ac3391179
Guilherme Camboim <105326604+guilhermecamboim@users.noreply.github.com>
2022-10-30 13:57:48 -0300

    Refatorando Card da Home.

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,4 +1,6 @@
+import { Trash } from 'phosphor-react';
 import { NavLink } from 'react-router-dom';
+import { api } from '../../lib/axios';
 import styles from './styles.module.css';
 
 interface IProps {
@@ -8,14 +10,25 @@ interface IProps {
   navLink: any;
 }
 
+
 export function Card({title, subtitle, image, navLink}: IProps) {
+  
+  function handleDeletePost(){
+    const deleteData = api.delete(`post/${navLink}`)
+
+    window.location.reload();
+  }
+
   return (
-    <NavLink className={styles.containerCard} to={`posts/${navLink}`}>
+    <div className={styles.containerCard}>
+      <Trash size={26} className={styles.trashButton} onClick={handleDeletePost}/>
+      <NavLink className={styles.containerLink} to={`posts/${navLink}`}>
         <img className={styles.imgCard} src={image}/>
         <div className={styles.contentCard}>
           <h1>{title}</h1>
           <p>{subtitle}</p>
         </div>
-    </NavLink>
+      </NavLink>
+    </div>
   )
 }
