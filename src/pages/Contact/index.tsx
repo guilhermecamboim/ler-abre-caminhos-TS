c49045714dd0a6dfea861e93bea86648969f1575
Guilherme Camboim <105326604+guilhermecamboim@users.noreply.github.com>
2022-10-31 20:16:25 -0300

    Criando página de contato.

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,5 +1,111 @@
+import { useState } from 'react';
+
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
+import * as S from './styles'
+import { X } from "phosphor-react";
+import { api } from '../../lib/axios';
+
+
+interface IFormPostProps  {
+  name: string;
+  email: string;
+  yourMessage: string;
+}
+
 export function Contact(){
+
+  const [submitedPost, setSubmitedPost] = useState<any>(false);
+
+  const newPostFormSchema = zod.object({
+    name: zod.string().min(1, 'Informe um título válido.').max(100, 'O título deve ser inferior a 100 caracteres.'),
+    email: zod.string().min(1, 'Informe um parágrafo válido.').max(500, 'O parágrafo deve ser inferior a 500 caracteres.'),
+    yourMessage: zod.string().min(1, 'Informe um parágrafo válido.').max(500, 'O título deve ser inferior a 500 caracteres.'),
+  })
+
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: zodResolver(newPostFormSchema)
+  })
+
+  function handleCreateNewPost(data: IFormPostProps | any) {
+
+    const title = data.title
+    const firstParagraph = data.firstParagraph
+    const secondParagraph = data.secondParagraph
+    const thirdParagraph = data.thirdParagraph
+    const impactPhrase = data.impactPhrase
+    const authorLink = data.authorLink
+    const authorSocialMedia = data.authorSocialMedia
+
+    const result = api.post('post', {
+      title,
+      firstParagraph,
+      secondParagraph,
+      thirdParagraph,
+      impactPhrase,
+      authorLink,
+      authorSocialMedia,
+    })
+
+    setSubmitedPost(true)
+
+  }
+
+
+
   return(
-    <h1>Contato</h1>
+    <S.ContainerAbout>
+
+        <S.Title>Entre em contato com a gente!</S.Title>
+
+          <form onSubmit={handleSubmit(handleCreateNewPost)} action="">
+        <S.ContainerForm>
+            <h3>Nome</h3>
+            <S.TextInputContainer>
+              <input 
+                placeholder="Digite o seu nome."
+                {...register('name')}
+              />
+            </S.TextInputContainer>
+            <S.ErrorMessage >
+              <>
+                {errors.title?.message}
+              </>
+            </S.ErrorMessage>
+
+            <h3>Email</h3>
+            <S.TextInputContainer>
+              <input 
+                placeholder="Digite o seu e-mail."
+                {...register('email')}
+              />
+            </S.TextInputContainer>
+            <S.ErrorMessage>
+              <>
+                {errors.title?.message}
+              </>
+            </S.ErrorMessage>
+
+            <h3>Mensagem</h3>
+            <textarea 
+              placeholder="Digite sua mensagem."
+              {...register('yourMessage')}
+            />
+            <S.ErrorMessage>
+              <>
+                {errors.firstParagraph?.message}
+              </>
+            </S.ErrorMessage>
+
+            <button type="submit">Enviar mensagem</button>
+        </S.ContainerForm>
+          </form>
+        <S.PopUpSuccess color={submitedPost}>
+          <p>Mensagem enviada com sucesso</p>
+          <X size={18} onClick={() => setSubmitedPost(false)}/>
+        </S.PopUpSuccess>
+    </S.ContainerAbout>
   )
 }
--- src/pages/Contact/styles.ts
+++ src/pages/Contact/styles.ts
@@ -1,1 +1,131 @@
 import styled from 'styled-components'
+
+export const ContainerAbout = styled.div `
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 82rem;
+  height: 40rem;
+`
+
+export const Title = styled.h2 `
+  margin-bottom: 2rem;
+
+  position: relative;
+  right: 125px;
+`
+
+export const ContainerForm = styled.div `
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+
+  align-items: flex-start;
+  width: 40rem;
+
+  textarea {
+  width: 80%;
+  background: var(--gray-800);
+  border: 0;
+  resize: none;
+  height: 10rem;
+  padding: 1rem;
+  border-radius: 8px;
+  color: var(--white);
+  line-height: 1.6;
+  margin-top: 1rem;
+  border: 2px solid var(--violet-900);
+
+}
+
+textarea:focus {
+  border-color: var(--violet-400);
+}
+
+textarea::placeholder {
+  color: var(--gray-400);
+}
+
+  button[type='submit'] {
+  margin-top: 50px;
+  font-weight: bold;
+  color: var(--white);
+  border: 2px solid var(--violet-900);
+  background: var(--violet-700);
+  padding: 1rem;
+  border-radius: 8px;
+  cursor: pointer;
+  width: 80%;
+}
+
+button[type='submit']:hover {
+  background: var(--violet-500);
+  transition: 1s;
+}
+
+`
+
+export const TextInputContainer = styled.div `
+
+  input {
+  background: var(--gray-800);
+  padding: 12px 16px;
+  border-radius: 8px;
+  box-sizing: border-box;
+  border: 2px solid var(--violet-900);
+  display: flex;
+  align-items: baseline;
+  width: 500px;
+}
+
+  input:focus {
+  border-color: var(--violet-400)
+  }
+
+  input:disabled {
+  opacity: 0.5;
+  cursor: not-allowed;
+  }
+
+  input:focus {
+  outline: none !important;
+  box-shadow: none !important;
+
+  }
+
+  input:disabled {
+    cursor: not-allowed;
+
+  }
+
+  input::placeholder {
+    color: var(--gray-400);
+
+  }
+`
+
+export const ErrorMessage = styled.p  `
+  color: var(--red-500);
+`
+
+export const PopUpSuccess = styled.div`
+  width: 400px;
+  height: 125px;
+  border-radius: 8px;
+  position: fixed;
+  right: 0;
+  top: 0;
+  background: var(--green-300);
+  color: var(--gray-900);
+  display: ${(props) => props.color ? 'flex' : 'none'};
+  justify-content: center;
+  align-items: center;
+
+  svg {
+    position: relative;
+    top: -40px;
+    right: -80px;
+    cursor: pointer;
+  }
+`
